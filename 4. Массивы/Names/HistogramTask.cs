3bd7f1e53b646b2efa94b30f00865fadfde50e3e
Maksim Kolosov <maximum.0716@mail.ru>
2024-04-06 13:48:59 +0500

    complete histogram task

---
+++ 4. Массивы/Names/HistogramTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Names;
+
+internal static class HistogramTask
+{
+    private const int AmountOfDaysInMonth = 31;
+    
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var xLabels = GetDays();
+
+        var yLabels = GetBirthsPerDay(names, name);
+
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'",
+            xLabels,
+            yLabels);
+    }
+
+    public static string[] GetDays()
+    {
+        var days = new string[AmountOfDaysInMonth];
+
+        for (var i = 0; i < days.Length; i++)
+            days[i] = (i + 1).ToString();
+
+        return days;
+    }
+    
+    public static double[] GetBirthsPerDay(NameData[] names, string name)
+    {
+        var birthsCounts = new double[AmountOfDaysInMonth];
+
+        var withRequiredName = names
+            .Where(n => n.BirthDate.Day != 1 && n.Name == name);
+            
+        foreach (var nameData in withRequiredName)
+            birthsCounts[nameData.BirthDate.Day - 1]++;
+
+        return birthsCounts;
+    }
+}
